4dae89bf9cad319dd8f86d160307f415420a27bd
Matt Brzezinski <matt.brzezinski@invenia.ca>
2021-11-05 11:55:52 -0500

    mktempdir() instead of mkdir() and rm()

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,10 @@ using LambdaMaker
 using Test
 
 SAMPLE_FILENAME = "Dockerfile"
+PROJECT_NAME = "foobar"
 
 @testset "_copy_file" begin
-    RESOURCE_DIR = mkdir("resources")
-
-    try
+    mktempdir() do RESOURCE_DIR
         file_path = joinpath(RESOURCE_DIR, SAMPLE_FILENAME)
 
         @testset "dest file does not exist" begin
@@ -22,56 +21,61 @@ SAMPLE_FILENAME = "Dockerfile"
             LambdaMaker._copy_file(RESOURCE_DIR, SAMPLE_FILENAME; force=true)
             @test isfile(file_path)
         end
-    finally
-        rm(RESOURCE_DIR; force=true, recursive=true)
     end
 end
 
 @testset "_render_file" begin
-    RESOURCE_DIR = mkdir("resources")
-    try
-        render_args = Dict("PKG" => "foobar")
-
+    mktempdir() do RESOURCE_DIR
+        render_args = Dict("PKG" => PROJECT_NAME)
         LambdaMaker._render_file(SAMPLE_FILENAME, RESOURCE_DIR, render_args)
 
         result_file_path = joinpath(RESOURCE_DIR, SAMPLE_FILENAME)
         @test isfile(result_file_path)
-    finally
-        rm(RESOURCE_DIR; force=true, recursive=true)
+
+        contents = read(result_file_path, String)
+        @test contains(contents, "CMD [\"$(PROJECT_NAME).handle_event\"]")
     end
 end
 
 
 @testset "create_lambda_package" begin
-    project_name = "foobar"
+    function get_project_files()
+        for (_, _, files) in walkdir(joinpath(@__DIR__, "..", "template_files"))
+            return files
+        end
+    end
+
+    function test_file_existance()
+        for project_file in get_project_files()
+            @test isfile(joinpath(PROJECT_NAME, project_file))
+        end
+    end
 
     @testset "project does not exist" begin
         try
-            create_lambda_package(project_name)
-
-            @test isdir(project_name)
-            @test isfile(joinpath(project_name), "Project.toml")
+            create_lambda_package(PROJECT_NAME)
+            test_file_existance()
         finally
-            rm(project_name; force=true, recursive=true)
+            rm(PROJECT_NAME; force=true, recursive=true)
         end
     end
 
     @testset "project exists & force=false" begin
         try
-            mkdir(project_name)
-            @test_throws DirectoryExists create_lambda_package(project_name)
+            mkdir(PROJECT_NAME)
+            @test_throws DirectoryExists create_lambda_package(PROJECT_NAME)
         finally
-            rm(project_name)
+            rm(PROJECT_NAME)
         end
     end
 
     @testset "project exists & force=true" begin
         try
-            mkdir(project_name)
-            create_lambda_package(project_name; force=true)
-            @test isdir(project_name)
+            mkdir(PROJECT_NAME)
+            create_lambda_package(PROJECT_NAME; force=true)
+            test_file_existance()
         finally
-            rm(project_name; force=true, recursive=true)
+            rm(PROJECT_NAME; force=true, recursive=true)
         end
     end
 end
